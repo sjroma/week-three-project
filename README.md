32a6789faa01720963d95ae5905ff3aca23de22f
Steve Roma <sjroma@hotmail.com>
2017-06-11 16:53:25 -0400

    jsCoding: start of main.js with comments/pseudo code, removed unused eval class from css

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Build a Calculator
 
-* This exercise will help you understand how to use JavaScript functions as well as to start interacting with the DOM.
+* This exercise will help you understand how to use JavaScript functions as well as to start interacting with the DOM (Document Object Model).
---
+++ main.js
@@ -0,0 +1,34 @@
+// Get all the operator keys from document
+
+
+
+// Add onclick event to all the keys and perform operations
+
+
+
+// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
+// If clear key is pressed, erase everything
+
+
+
+// If eval key is pressed, calculate and display the result
+
+
+
+// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+
+
+
+// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
+
+
+
+// Basic functionality of the calculator is complete. But there are some problems like 
+// 1. No two operators should be added consecutively.
+// 2. The equation shouldn't start from an operator except minus
+// 3. not more than 1 decimal should be there in a number
+		
+// We'll fix these issues using some simple checks
+
+
+
