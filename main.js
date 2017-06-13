33db5c1cca91e4d7414655f71705ec39530f234b
Steve Roma <sjroma@hotmail.com>
2017-06-13 18:17:57 -0400

    initial add of javascript, more IDs named

--- main.js
+++ main.js
@@ -14,3 +14,40 @@
 // 1. No two operators should be selected consecutively
 // 2. The equation shouldn't start with an operator except minus
 // 3. No more than one decimal should be in a number
+
+//let stuffThanNeedsEventListeners = document.querySelectorAll('className')
+//.forEach(function(element){
+//	element.addEvenListener("click", function(){
+//		//stuff to do on click
+//	})
+//});
+
+var nums = document.getElementsByClassName("num");
+var screen = document.getElementById("display");
+
+var total = 0;
+var tempNumber = "";
+var operator;
+
+for (var num of nums) {
+  num.addEventListener('click', handleClick);
+}
+
+function handleClick(event) {
+  tempNumber += event.target.value;
+  results.value = tempNumber;
+  console.log(tempNumber);
+  
+  document.getElementById('add').addEventListener
+    op = '+';
+    total += parseInt(tempNumber);
+    tempNumber = "";
+};
+
+document.getElementById('equals').addEventListener
+  if(op === "+") {
+    total += parseInt(tempNumber);
+  }
+results.value = total;
+tempNumber = "";
+
